f736ff534f5e1c5e165d896488fbabda61e1127d
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-02-15 16:23:47 +0530

    starting with express task 19

--- main.js
+++ main.js
@@ -1,130 +1,123 @@
-const amount = document.getElementById('amount');
-const description = document.getElementById('description');
-const category = document.getElementById('category');
-const expenseList = document.getElementById('expense-list');
-const endpointId = 'e1ff4cd9712d4c6990512de274016515';
-const serverLink = `https://crudcrud.com/api/${endpointId}/expense`;
-let id = null;
+const amount = document.getElementById("amount");
+const description = document.getElementById("description");
+const category = document.getElementById("category");
+const expenseList = document.getElementById("expense-list");
+const serverLink = `http://localhost:4000`;
 
-addEventListener('DOMContentLoaded', () => {
-    [...document.getElementsByClassName('option')].forEach(option => {
-        option.addEventListener('click', (e) => {
-            category.textContent = option.textContent;
-        })
-    });
+addEventListener("DOMContentLoaded", () => {
+  const addingEventListernerToOptionsDropdown = () => {
+    const options = document.querySelectorAll(".option");
+    for (let i = 0; i < options.length; i++) {
+      options[i].addEventListener("click", (e) => {
+        category.textContent = options[i].textContent;
+      });
+    }
+  };
 
-    const getData = async () => {
-        try{
-            const res = await axios.get(serverLink);
-            const expenses = res.data;
-            for(let i=0; i<expenses.length; i++){
-                expenseList.appendChild(createLi(expenses[i]));
-            }
-        }catch(err){
-            console.log(err);
-        }
-    };
-    getData();
+  const getExpenseList = async () => {
+    try {
+      const res = await axios.get(serverLink);
+      const expenses = res.data;
+      for (let i = 0; i < expenses.length; i++) {
+        expenseList.appendChild(createLi(expenses[i]));
+      }
+      console.log("Received Expenses from db");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  addingEventListernerToOptionsDropdown();
+  getExpenseList();
 });
 
-document.getElementById('form').addEventListener('submit', addExpense);
+document.getElementById("form").addEventListener("submit", addExpense);
 
-function addExpense(e){
-    e.preventDefault();
-    let expense = {
-        amount: amount.value,
-        description: description.value,
-        category: category.textContent
-    }
-    if(isInputsMissing(expense)){
-        return;
-    }    
-    
-    const postData = async () => {
-        try{
-            let res;
-            if(id == null){
-                res = await axios.post(serverLink, expense);
-                expense = res.data;
-            }else{
-                res = await axios.put(`${serverLink}/${id}`, expense);
-                expense._id = id;
-                id = null;
-            }
-            console.log(res);
-            expenseList.appendChild(createLi(expense));
-        }catch(err){
-            console.log(err);
-        }
-    }
-    postData();
+function addExpense(e) {
+  e.preventDefault();
 
-    document.getElementById('amount').value = '';
-    document.getElementById('description').value = '';
-    category.textContent = 'Categories';
+  let expense = {
+    amount: amount.value,
+    description: description.value,
+    category: category.textContent,
+  };
+
+  if (isInputsMissing(expense)) {
+    return;
+  }
+
+  const postNewExpense = async () => {
+    try {
+      const res = await axios.post(serverLink, expense);
+      expense = res.data;
+      expenseList.appendChild(createLi(expense));
+      console.log("New Expense Added");
+      console.log(expense);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const settingInputValuesToDefault = () => {
+    amount.value = "";
+    description.value = "";
+    category.textContent = "Categories";
+  };
+
+  postNewExpense();
+  settingInputValuesToDefault();
 }
 
 function createLi(expense) {
-    const li = document.createElement('li');
-    li.textContent = `${expense.amount}-${expense.category}-${expense.description}`;  
-    li.id = expense._id;
-    li.appendChild(createDeleteButton());
-    li.appendChild(createEditButton());
-    return li;
+  const li = document.createElement("li");
+  li.textContent = `${expense.amount}-${expense.category}-${expense.description}`;
+  li.id = expense.id;
+  li.className = "mt-3";
+  li.style.width = "500px";
+  li.appendChild(createDeleteButton());
+  return li;
 }
 
-function createDeleteButton(){
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete Expense';
-    deleteButton.className = 'del btn btn-danger';
-    deleteButton.addEventListener('click', deleteLi);
-    return deleteButton;
-}
-
-function createEditButton(){
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Edit Expense';
-    editButton.className = 'edit btn btn-warning';
-    editButton.addEventListener('click', editLi);
-    return editButton;
+function createDeleteButton() {
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "del btn btn-danger float-end";
+  deleteButton.addEventListener("click", deleteLi);
+  return deleteButton;
 }
 
 function deleteLi(e) {
-    const deleteData = async () => {
-        try{
-            const res = await axios.delete(`${serverLink}/${e.path[1].id}`);
-            console.log(res);
-        }catch(err){
-            console.log(err);
-        }
-    };
-    deleteData();
-    e.path[1].remove();
+  const deleteData = async () => {
+    try {
+      const res = await axios.delete(
+        `${serverLink}/${e.target.parentElement.id}`
+      );
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  deleteData();
+  e.target.parentElement.remove();
 }
 
-function editLi(e) {
-    const contents = e.path[1].textContent.slice(0, -26).split('-');
-    amount.value = contents[0];
-    description.value = contents[2];
-    category.textContent = contents[1];
-    id = e.path[1].id;
-    e.path[1].remove();
-}
+function isInputsMissing(expense) {
+  let missingInputs = document.getElementById("missing-inputs");
 
-function isInputsMissing(expense){
-    let missingInputs = document.getElementById('missing-inputs');
-    missingInputs.textContent = '';
-    if(expense.amount === ''){
-        missingInputs.textContent += 'Amount is missing.';
-    }
-    if(expense.description === ''){
-        missingInputs.textContent += ' Description is missing.';
-    }
-    if(expense.category === 'Categories'){
-        missingInputs.textContent += ' Choose a category.';
-    }
-    if(missingInputs.textContent == ''){
-        return false;
-    }
-    return true;
+  missingInputs.textContent = "";
+
+  if (expense.amount === "") {
+    missingInputs.textContent += "Amount is missing.";
+  }
+  if (expense.description === "") {
+    missingInputs.textContent += " Description is missing.";
+  }
+  if (expense.category === "Categories") {
+    missingInputs.textContent += " Choose a category.";
+  }
+
+  if (missingInputs.textContent == "") {
+    return false;
+  }
+  return true;
 }
